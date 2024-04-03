4990c497975aa42c3adb50309c43af284adfd7c5
nprange <nprange@iastate.edu>
2024-04-03 15:10:34 -0500

    Animated home page

---
+++ app.js
@@ -0,0 +1,25 @@
+var id = null;
+function loadPage() {
+    var name = document.getElementById("title1");
+    var bio = document.getElementById("bio1");
+    var namePos = 0;
+    var bioPos = -68;
+
+    clearInterval(id);
+    id = setInterval(frame, 15);
+    function frame() {
+        if (namePos == 38) {
+            clearInterval(id);
+        } else {
+            namePos++;
+            name.style.left = namePos + '%';
+        }
+        if (bioPos == 38) {
+            clearInterval(id);
+        } else {
+            bioPos++;
+            bio.style.right = bioPos + '%';
+        }
+    }
+} 
+ 

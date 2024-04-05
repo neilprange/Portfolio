15eb4bbc6af485302bb4c78f652e7aabe6797ec0
nprange <nprange@iastate.edu>
2024-04-05 16:18:16 -0500

    Updated home page

--- app.js
+++ app.js
@@ -3,22 +3,22 @@ function loadPage() {
     var name = document.getElementById("title1");
     var bio = document.getElementById("bio1");
     var namePos = 0;
-    var bioPos = -69;
+    var bioPos = 100;
 
     clearInterval(id);
     id = setInterval(frame, 15);
     function frame() {
-        if (namePos == 38) {
+        if (namePos == 50) {
             clearInterval(id);
         } else {
             namePos++;
             name.style.left = namePos + '%';
         }
-        if (bioPos == 38) {
+        if (bioPos == 50) {
             clearInterval(id);
         } else {
-            bioPos++;
-            bio.style.right = bioPos + '%';
+            bioPos--;
+            bio.style.left = bioPos + '%';
         }
     }
 } 
